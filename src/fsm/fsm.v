ca5c21e36b630fc8caa30f9b8667f3f27b95687c
jk2102 <jurica.kundrata@gmail.com>
2024-03-12 19:02:43 +0100

    Update fsm.v

--- src/fsm/fsm.v
+++ src/fsm/fsm.v
@@ -23,5 +23,183 @@ module fsm (
     input   rstn
 );
 
+    
+   parameter <state1>  = 4'b0000;
+   parameter <state2>  = 4'b0001;
+   parameter <state3>  = 4'b0010;
+   parameter <state4>  = 4'b0011;
+   parameter <state5>  = 4'b0100;
+   parameter <state6>  = 4'b0101;
+   parameter <state7>  = 4'b0110;
+   parameter <state8>  = 4'b0111;
+   parameter <state9>  = 4'b1000;
+   parameter <state10> = 4'b1001;
+   parameter <state11> = 4'b1010;
+   parameter <state12> = 4'b1011;
+   parameter <state13> = 4'b1100;
+   parameter <state14> = 4'b1101;
+   parameter <state15> = 4'b1110;
+   parameter <state16> = 4'b1111;
+
+   reg [3:0] <state> = <state1>;
+
+   always @(posedge <clock>)
+      if (<reset>) begin
+         <state> <= <state1>;
+         <outputs> <= <initial_values>;
+      end
+      else
+         case (state)
+            <state1> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state2> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state3> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state4> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state5> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state6> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state7> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state8> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state9> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state10> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state11> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state12> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state13> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state14> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state15> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            <state16> : begin
+               if (<condition>)
+                  <state> <= <next_state>;
+               else if (<condition>)
+                  <state> <= <next_state>;
+               else
+                  <state> <= <next_state>;
+               <outputs> <= <values>;
+            end
+            default : begin  // Fault Recovery
+               <state> <= <state1>;
+               <outputs> <= <values>;
+            end
+         endcase
+				
+
 
 endmodule
