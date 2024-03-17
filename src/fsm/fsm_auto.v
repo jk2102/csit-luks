048e6b3bb80b76e8b12f60182f5d3dc7da33e79a
Jan Furlan <jan.furlan@ericsson.com>
2024-03-17 14:48:17 +0100

    fsm: added first version
    - added an additional (untested) version
      which implements auto mode

--- src/fsm/fsm_auto.v
+++ src/fsm/fsm_auto.v
@@ -38,7 +38,7 @@ module fsm (
    output reg [3:0] SS_val,
    output reg [3:0] F_val,
    output reg [2:0] EXP_val,
-   output reg [1:0] input_sel
+   output reg [1:0] input_sel,
 );
 
 
@@ -56,41 +56,42 @@ module fsm (
 
    reg [3:0] current_state;
    reg [3:0] previous_state;
-   reg F_set_flag;
+   reg [3:0] last_normal_state;
+   reg [3:0] last_auto_state;
 
    always @(posedge clk)
       if (rstn) begin
          current_state <= IDLE;
          previous_state <= IDLE;
+         last_normal_state <= SS_SEL;
          fd_address <= 8'b00000000;
          fd_valid <= 1'b0;
          ISO_val <= 4'b1000;
          SS_val <= 4'b1000;
          F_val <= 4'b1000;
          EXP_val <= 3'b000;
-         input_sel <= 2'b00;
+         selectInput <= 2'b00;
          lux_valid <= 1'b0;
-         F_set_flag <= 1'b0;
       end
       else
          case (current_state)
             IDLE: begin
                current_state <= ISO_SEL;
-               previous_state <= IDLE;
                fd_address <= 8'b00000000;
                fd_valid <= 1'b0;
                ISO_val <= 4'b1000;
                SS_val <= 4'b1000;
                F_val <= 4'b1000;
                EXP_val <= 3'b000;
-               input_sel <= 2'b00;
+               selectInput <= 2'b00;
                lux_valid <= 1'b0;
-               F_set_flag <= 1'b0;
             end
 
             ISO_SEL: begin
-               if (pb_press == 2'b10)
+               if (pb_press == 2'b01)
                   current_state <= SS_SEL;
+               else if (pb_press == 2'b11)
+                  current_state <= AUTO_MODE;
                else
                   current_state <= ISO_SEL;
                ISO_val <= enc_count;
@@ -100,35 +101,34 @@ module fsm (
             SS_SEL: begin
                if (pb_press == 2'b01)
                   current_state <= F_SEL;
-               else if (pb_press == 2'b10 && F_set_flag)
-                  current_state <= EXP_METER;
                else if (pb_press == 2'b11)
-                  current_state <= ISO_SEL;
+                  current_state <= AUTO_MODE;
                else
                   current_state <= SS_SEL;
-               previous_state <= SS_SEL;
+               last_normal_state <= SS_SEL
                SS_val <= enc_count;
                input_sel <= 2'b01;
             end
 
             F_SEL: begin
                if (pb_press == 2'b01)
-                  current_state <= SS_SEL;
-               else if (pb_press == 2'b10)
                   current_state <= EXP_METER;
+               else if (pb_press == 2'b10)
+                  current_state <= SS_SEL;
                else if (pb_press == 2'b11)
-                  current_state <= ISO_SEL;
+                  current_state <= AUTO_MODE;
                else
                   current_state <= F_SEL;
-               F_set_flag = 1'b1;
-               previous_state <= F_SEL;
+               last_normal_state <= F_SEL;
                SS_val <= enc_count;
                input_sel <= 2'b10;
             end
 
             EXP_METER: begin
-               if (lux_ready == 1'b1)
+               if (lux_ready == 1'b1 && previous_state == F_SEL)
                   current_state <= EXP_LUT;
+               else if (lux_ready == 1'b1 && previous_state == AUTO_MODE)
+                  current_state <= AUTO_LUT;
                else
                   current_state <= EXP_METER;
                lux_valid <= 1'b1;
@@ -147,13 +147,67 @@ module fsm (
                if(pb_press == 2'b01)
                   current_state <= EXP_METER;
                else if (pb_press == 2'b10)
-                  current_state <= previous_state;
+                  current_state <= SS_SEL;
                else if (pb_press == 2'b11)
-                  current_state <= ISO_SEL;
+                  last_normal_state <= SS_SEL;
+                  current_state <= AUTO_MODE;
                else
                   current_state <= EXP_DISP;
                input_sel <= 2'b11;
                EXP_val <= fd;
+            end
+
+            AUTO_MODE: begin
+               if (pb_press == 2'b01)
+                  previous_state <= AUTO_MODE;
+                  current_state <= EXP_METER;
+               else if (pb_press == 2'b11)
+                  current_state <= last_normal_state;
+               else
+                  current_state <= AUTO_MODE;
+               input_sel <= 2'b11;
+               EXP_val <= 3'b101;
+            end
+
+            AUTO_LUT: begin
+               if (fd_ready && (last_auto_state == AUTO_DISP_SS))
+                  current_state <= AUTO_DISP_SS;
+               else if (fd_ready && (last_auto_state == AUTO_DISP_F))
+                  current_state <= AUTO_DISP_F;
+               else
+                  current_state <= AUTO_LUT;
+               fd_address <= ({ISO_val, LUX_val} || (24'b000000000000000000001000));
+               fd_valid <= 1'b1;
+            end
+
+            AUTO_DISP_SS: begin
+               if (pb_press == 2'b01)
+                  current_state <= AUTO_DISP_F;
+               else if (pb_press == 2'b10)
+                  previous_state <= AUTO_MODE;
+                  last_auto_state <= AUTO_DISP_SS,
+                  current_state <= EXP_METER;
+               else if (pb_press == 2'b11)
+                  current_state <= last_normal_state;
+               else
+                  current_state <= AUTO_DISP_SS;
+               SS_val <= fd[3:0];
+               input_sel <= 2'b01;
+            end
+
+            AUTO_DISP_F: begin
+               if (pb_press == 2'b01)
+                  current_state <= AUTO_DISP_SS;
+               else if (pb_press == 2'b10)
+                  previous_state <= AUTO_MODE;
+                  last_auto_state <= AUTO_DISP_F;
+                  current_state <= EXP_METER;
+               else if (pb_press == 2'b11)
+                  current_state <= last_normal_state;
+               else
+                  current_state <= AUTO_DISP_F;
+               F_val <= fd[7:4];
+               input_sel <= 2'b10;
             end
 
             default : begin  // Fault Recovery
