ca8db69cc75c40e42985fee8b85a34229f9cc759
Tin Soric <tin.soric97@gmail.com>
2024-04-07 09:38:03 +0200

    Fixed wrapper ports

--- src/spi_flash/spi_wrapper.v
+++ src/spi_flash/spi_wrapper.v
@@ -5,11 +5,13 @@ module spi_wrapper (
 	input clk_i,
 	input rstn_i,
 	input valid_i,
-	input [3:0] cfgreg_we,
 	output ready_o,
 	input [23:0] addr_i,
-	input [31:0] wrdata_i,
-	output wire [7:0] rdata_o
+	output wire [7:0] rdata_o,
+	output spi_clk,
+	input miso,
+	output mosi,
+	output csb
 );
 
 
@@ -30,21 +32,8 @@ wire flash_io3_oe;
 wire flash_io0_do;
 wire flash_io1_do;
 wire flash_io2_do;
-wire flsah_io3_do;
-wire cfgreg_we;
-reg [31:0] cfgreg_di;
-
-
-
-
-spiflash spiflash_inst_0 (
-	.csb(csb),
-	.clk(clock), 
-	.io0(flash_io0), //MOSI
-	.io1(flash_io1), //MISO
-	.io2(flash_io2),
-	.io3(flash_io3)
-);
+wire flash_io3_do;
+wire [3:0] cfgreg_we = 4'b0000;
 
 
 spimemio spimemio_inst_0 (
@@ -57,25 +46,25 @@ spimemio spimemio_inst_0 (
 	.rdata(rdata_o),
 
 	.flash_csb(csb),
-	.flash_clk(clock),
+	.flash_clk(spi_clk),
 
 	.flash_io0_oe(flash_io0_oe),
 	.flash_io1_oe(flash_io1_oe),
 	.flash_io2_oe(flash_io2_oe),
 	.flash_io3_oe(flash_io3_oe),
 
-	.flash_io0_do(flash_io0_do),
+	.flash_io0_do(mosi),
 	.flash_io1_do(flash_io1_do),
 	.flash_io2_do(flash_io2_do),
 	.flash_io3_do(flash_io3_do),
 
-	.flash_io0_di(flash_io0),
-	.flash_io1_di(flash_io1),
+	.flash_io0_di(),
+	.flash_io1_di(miso),
 	.flash_io2_di(flash_io2),
 	.flash_io3_di(flash_io3),
 
 	.cfgreg_we(cfgreg_we),
-	.cfgreg_di(wrdata_i),
+	.cfgreg_di(),
 	.cfgreg_do()
 );
 
