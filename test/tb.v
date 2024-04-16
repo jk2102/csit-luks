633b65003847b763b324701ee457acf2831e0ebf
jk2102 <jurica.kundrata@gmail.com>
2024-04-16 16:20:43 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -14,7 +14,7 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg rstn;
+  reg rst_n;
   reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
@@ -38,7 +38,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n   (rstn)      // not reset
+     .rst_n   (rst_n)      // not reset
   );
 
 // // Clock generation
